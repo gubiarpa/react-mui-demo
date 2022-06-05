c376f539d92055055f583b2632ae4c94eeb6da0a
Billy Arredondo <billy.arredondo@pucp.pe>
2022-06-05 18:58:58 -0500

    MuiButton - Part 1
    Adding Button component

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,12 @@
+import { Stack, Button } from '@mui/material'
+
+export const MuiButton = () => {
+    return (
+        <>
+            <Button variant='text'>Text</Button>
+            <Button variant='contained'>Contained</Button>
+            <Button variant='outlined'>Outlined</Button>
+        </>
+    )
+}
+
