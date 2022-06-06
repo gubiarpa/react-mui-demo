ab53170a64e68545daf97de0d10cbec393dc881a
Billy Arredondo <billy.arredondo@pucp.pe>
2022-06-05 19:23:33 -0500

    MuiButton - Part 2

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -2,11 +2,26 @@ import { Stack, Button } from '@mui/material'
 
 export const MuiButton = () => {
     return (
-        <>
-            <Button variant='text'>Text</Button>
-            <Button variant='contained'>Contained</Button>
-            <Button variant='outlined'>Outlined</Button>
-        </>
+        <Stack spacing={4}>
+            <Stack spacing={2} direction='row'>
+                <Button variant='text' href='https://google.com'>Text</Button>
+                <Button variant='contained'>Contained</Button>
+                <Button variant='outlined'>Outlined</Button>
+            </Stack>
+            <Stack spacing={2} direction='row'>
+                <Button variant='outlined' color='primary'>Primary</Button>
+                <Button variant='outlined' color='secondary'>Secondary</Button>
+                <Button variant='outlined' color='error'>Error</Button>
+                <Button variant='outlined' color='warning'>Warning</Button>
+                <Button variant='outlined' color='info'>Info</Button>
+                <Button variant='outlined' color='success'>Success</Button>
+            </Stack>
+            <Stack display='block' spacing={2} direction='row'>
+                <Button variant='contained' size='small'>Small</Button>
+                <Button variant='contained' size='medium'>Medium</Button>
+                <Button variant='contained' size='large'>Large</Button>
+            </Stack>
+        </Stack>
     )
 }
 
